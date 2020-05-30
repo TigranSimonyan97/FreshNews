171b0237cd2bdb2b5b068c3affa1b75f95cc1033
Tigran2662 <tigran.simonyan@picsart.com>
2020-05-31 00:35:29 +0400

    Refactored cache cleaning

--- FreshNews/Caching/ImageCacheManager.swift
+++ FreshNews/Caching/ImageCacheManager.swift
@@ -10,16 +10,33 @@ import SwiftUI
 import Combine
 
 protocol ImageCacheable : Cacheable where ObjectType == UIImage {
-    
+    var cachedObjectsCount: Int? { get }
+    var lastCleanupDate: Date? { get }
 }
 
 class ImageCacheManager: ImageCacheable {
     typealias ObjectType = UIImage
     
+    private static let lastCleanupDateKey = "lastCleanupDateKey"
+    
     private let urlProvider = ImageURLProvider()
 
     private let directory: String
     private let imageRelativePath: String
+    
+    var cachedObjectsCount: Int? {
+        let fileManager = FileManager.default
+        let basePath = urlProvider.baseURL.appendingPathComponent(directory).absoluteString
+        return try? fileManager.contentsOfDirectory(atPath: basePath).count
+    }
+    
+    var lastCleanupDate: Date? {
+        UserDefaults.standard.string(forKey: ImageCacheManager.lastCleanupDateKey)?.toDate()
+    }
+    
+    var basePath: String {
+        urlProvider.baseURL.appendingPathComponent(directory).absoluteString
+    }
     
     init(directory: String, imageRelativePath: String) {
         self.directory = directory
@@ -27,6 +44,8 @@ class ImageCacheManager: ImageCacheable {
     }
      
     func cache(_ object: UIImage) {
+        clearCacheIfNeeded()
+        
         let relativePath = imageRelativePath.replacingOccurrences(of: "/", with: "")
 
         guard let data = object.pngData(), let url = urlProvider.url(for: directory, and: relativePath) else {
@@ -37,38 +56,48 @@ class ImageCacheManager: ImageCacheable {
         do {
             try data.write(to: url, options: .atomic)
         } catch let error {
-            print("some error \(error.localizedDescription), url \(url)")
+            print("Data write error \(error.localizedDescription), url \(url)")
         }
     }
 
     func clean() {
-//        let fileManager = FileManager.default
-//        let basePath = urlProvider.baseURL.absoluteString
-//        guard let filePaths = try? fileManager.contentsOfDirectory(atPath: basePath) else {
-//            return
-//        }
-
-//        let lastCleanupDateString = UserDefaults.standard.string(forKey: ImageCacheManager.lastCleanupDateKey)
-//        let lastCleanupDate = lastCleanupDateString?.toDate()
-//        let isADayPassed = lastCleanupDate != nil ? isOneDayPassed(from: lastCleanupDate!) : false
+        guard let cachedPaths = cachedFilesPaths() else {
+            print("There is no cached files in directory \(directory)")
+            return
+        }
         
-//        if filePaths.count > 200 ||  isADayPassed {
-//            do {
-//                try filePaths.forEach { try fileManager.removeItem(atPath: basePath + $0) }
-////                UserDefaults.standard.set(Date().toString(), forKey: ImageCacheManager.lastCleanupDateKey)
-//                print("Cached images cleaned up")
-//            } catch {
-//                print("Could not clear temp folder: \(error)")
-//            }
-//        }
+        do {
+            try cachedPaths.forEach { try FileManager.default.removeItem(atPath: basePath + $0) }
+            UserDefaults.standard.set(Date().toString(), forKey: ImageCacheManager.lastCleanupDateKey)
+            print("Cached images cleaned up")
+        } catch {
+            print("Could not clear temp folder: \(error)")
+        }
+
     }
     
-//    private func dayDiff(startDate: Date, endDate: Date) -> Int? {
-//        Calendar.current.dateComponents([.day], from: startDate, to: endDate).day
-//    }
-//
-//    func isOneDayPassed(from date: Date) -> Bool {
-//        guard let days = dayDiff(startDate: date, endDate: Date()) else { return false }
-//        return days > 1
-//    }
+    func clearCacheIfNeeded() {
+        let lastCleanupDateString = UserDefaults.standard.string(forKey: ImageCacheManager.lastCleanupDateKey)
+        let lastCleanupDate = lastCleanupDateString?.toDate()
+        let isDayPassedAfterLastCleanup = lastCleanupDate != nil ? isOneDayPassed(from: lastCleanupDate!) : false
+        
+        let cachedObjectsCountGreaterThenPermissible = cachedObjectsCount != nil ? cachedObjectsCount! > 200 : false
+        if cachedObjectsCountGreaterThenPermissible || isDayPassedAfterLastCleanup {
+            clean()
+        }
+    }
+    
+    //MARK: - Helpers
+    private func cachedFilesPaths() -> [String]? {
+        try? FileManager.default.contentsOfDirectory(atPath: basePath)
+    }
+    
+    private func dayDiff(startDate: Date, endDate: Date) -> Int? {
+        Calendar.current.dateComponents([.day], from: startDate, to: endDate).day
+    }
+
+    private func isOneDayPassed(from date: Date) -> Bool {
+        guard let days = dayDiff(startDate: date, endDate: Date()) else { return false }
+        return days > 1
+    }
 }
