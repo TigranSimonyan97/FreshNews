51ad132087349a47dc97c9144bdb9597a66b4cce
Tigran2662 <tigran.simonyan@picsart.com>
2020-05-30 17:34:19 +0400

    minor

--- FreshNews/Supporting Files/AppDelegate.swift
+++ FreshNews/Supporting Files/AppDelegate.swift
@@ -12,12 +12,9 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        ImageCacheManager.
         return true
     }
 
